4f33629819bd70d0850991a5b4a633689e936045
MilenaMaced <milenasantosmcd@gmail.com>
2018-04-02 17:49:59 -0300

    Adicionada a licença e correção do autor

--- src/main/java/model/classes/Locador.java
+++ src/main/java/model/classes/Locador.java
@@ -3,11 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model;
+
+package model.classes;
 
 /**
  *
- * @author BobaNote
+ * @author Milena Macedo - milenasantosmcd@gmail.com
  */
 public class Locador {
     
--- src/main/java/model/classes/Locatario.java
+++ src/main/java/model/classes/Locatario.java
@@ -3,11 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package model;
+package model.classes;
 
 /**
  *
- * @author BobaNote
+ * @author Milena Macedo - milenasantosmcd@gmail.com
  */
 public class Locatario {
     
