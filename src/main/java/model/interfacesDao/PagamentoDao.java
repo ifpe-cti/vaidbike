9f038347840c6deae8428437ae552f31588be190
Adrielly <adriellysales015@outlook.com>
2018-05-08 17:24:05 -0300

    Conforme a Issue #20
    Adicionado classe Pagamento Hibernate.

--- src/main/java/model/interfacesDao/PagamentoDao.java
+++ src/main/java/model/interfacesDao/PagamentoDao.java
@@ -19,27 +19,15 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
-*/
-
+ */
 package model.interfacesDao;
 
-import java.util.List;
 import model.classes.Pagamento;
 
 /**
  *
  * @author Adrielly Calado - adriellysales015@outlook.com
  */
-public interface PagamentoDao {
+public interface PagamentoDao extends Dao<Pagamento> {
 
-    public void inserir(Pagamento pagamento);
-    
-    public void recuperar(int codigo);
-    
-    public List <Pagamento> listarTodos ();
-    
-    
-    
-
-    
 }
