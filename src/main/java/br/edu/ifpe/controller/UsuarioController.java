9ceb20adec05aab2fab0de911c605a4d7f4f8da2
Milena Macedo <milenasantosmcd@gmail.com>
2018-08-04 19:47:10 -0300

    Conforme a Issue #134
    Adicionados os métodos de login no Usuário Controller.

--- src/main/java/br/edu/ifpe/controller/UsuarioController.java
+++ src/main/java/br/edu/ifpe/controller/UsuarioController.java
@@ -1,31 +1,75 @@
 package br.edu.ifpe.controller;
 
+import br.edu.ifpe.controller.md5.CriptografiaMD5;
 import br.edu.ifpe.model.classes.Endereco;
 import br.edu.ifpe.model.classes.Usuario;
 import br.edu.ifpe.model.validation.UsuarioModel;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-
+import javax.faces.context.FacesContext;
 
 @ManagedBean
 @SessionScoped
-public class UsuarioController{
-    
+public class UsuarioController {
+
     private Usuario cadUsuario;
+    private Usuario selectedUsuario;
     private Endereco end;
-    private final UsuarioModel USUARIOMODEL;
-    
+    private final UsuarioModel instaceUSUARIOMODEL;
+
     public UsuarioController() {
         this.end = new Endereco();
         cadUsuario = new Usuario();
-        this.USUARIOMODEL = new UsuarioModel();
+        this.instaceUSUARIOMODEL = new UsuarioModel();
     }
-    
-    public void registrarUsuario() throws Exception{
+
+    public Usuario getUsuarioLogado() {
+        return (Usuario) FacesContext.getCurrentInstance().getExternalContext()
+                .getSessionMap().get("usuarioLogado");
+    }
+
+    public void setUsuarioLogado(Usuario usuarioLogado) {
+        FacesContext.getCurrentInstance().getExternalContext()
+                .getSessionMap().put("usuarioLogado", usuarioLogado);
+    }
+
+    public String realizarLogin(String senha, String login) throws Exception {
+        Usuario user = null;
+        String rediricionarPagina = "";
+        senha = CriptografiaMD5.md5(senha);
+        user = instaceUSUARIOMODEL.recuperar(login, senha);
+
+        if (user != null) {
+
+            if (user.getSenha().equalsIgnoreCase(senha)) {
+                rediricionarPagina = "index.xhtml";
+
+            } else {
+                user = null;
+                FacesContext.getCurrentInstance().addMessage(null,
+                        new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falha no Login!",
+                                "Senha ou Login Inválidos"));
+
+            }
+        } else {
+            user = null;
+            FacesContext.getCurrentInstance().addMessage(null,
+                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Falha no Login!",
+                            "Senha ou Login Inválidos"));
+        }
+
+        return rediricionarPagina;
+    }
+
+    public void registrarUsuario() throws Exception {
+
+        cadUsuario.setSenha(CriptografiaMD5.md5(cadUsuario.getSenha()));
         cadUsuario.setEndereco(end);
-        this.USUARIOMODEL.inserir(this.cadUsuario);
+        this.instaceUSUARIOMODEL.inserir(this.cadUsuario);
         this.end = new Endereco();
         this.cadUsuario = new Usuario();
+
     }
 
     public Usuario getCadUsuario() {
@@ -44,5 +88,12 @@ public class UsuarioController{
         this.end = end;
     }
 
-    
+    public Usuario getSelectedUsuario() {
+        return selectedUsuario;
+    }
+
+    public void setSelectedUsuario(Usuario selectedUsuario) {
+        this.selectedUsuario = selectedUsuario;
+    }
+
 }
--- src/main/java/br/edu/ifpe/model/hibernate/UsuarioHibernate.java
+++ src/main/java/br/edu/ifpe/model/hibernate/UsuarioHibernate.java
@@ -80,10 +80,10 @@ public class UsuarioHibernate implements UsuarioDao {
 
         try {
             usuario = (Usuario) session.createQuery(
-                    "From Usuario where login = '" + login
-                    + "' and senha = '" + senha + "'").list().get(0);
+                    "From Usuario where login= '" + login
+                    + "' and senha= '" + senha + "'").list().get(0);
         } catch (Exception e) {
-            LOGGER.error("Ocorreu um problema ao recuperar o Usuario por login"
+            LOGGER.error("Ocorreu um problema ao recuperar o Usuario por login e senha!"
                     + "\n" + e.getMessage());
         } finally {
             session.close();
